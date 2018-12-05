da3a1e3b4aba30fb122b4545584bb9f29d99c945
Genie Yang <genieyang93@gmail.com>
2018-12-04 20:14:15 -0500

    Adding switch statement

--- liri.js
+++ liri.js
@@ -1,8 +1,11 @@
 //Read and set ant environmental variables with the dotnv package
 require('dotenv').config();
 
-//Inits Packages
+//Initial Node Packages
 const Spotify = require('node-spotify-api');
+const fs = require('fs');
+
+
 
 //Import API Keys froms key.js file
 let key = require("./keys.js");
@@ -12,5 +15,28 @@ let key = require("./keys.js");
 let input1 = process.argv[2];
 let input2 = process.argv[3];
 
+const findConcert = function(){
+    
+}
 
-//Initiate commands
+//Initiate switch commands
+
+switch (input1) 
+{
+    case 'concert-this':
+    findConcert();
+    break;
+
+    case 'spotify-this-song':
+    findSpotify();
+    break;
+
+    case 'movie-this':
+    findMovie();
+    break;
+
+    case 'do-what-it-says':
+    toDo();
+    break;
+
+}
